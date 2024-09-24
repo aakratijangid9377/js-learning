09b5d3f5161a1711381b13815b93e320d2d47a27
aakratijangid9377 <141710221+aakratijangid9377@users.noreply.github.com>
2024-09-24 23:46:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let username = 'Aakrati'
+let x = "which i don't have"
+// let userIntro = 'hii this is aakrati'
+let isHappy = false
+let userIntro = 'this is' 
+  + 'username'
+  + ''
 
-hey! Aakrati
