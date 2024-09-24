4c015c698d81e8057e9faa1afd92328b8e542690
aakratijangid9377 <141710221+aakratijangid9377@users.noreply.github.com>
2024-09-25 00:55:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let username = 'Aakrati'
 let x = "which i don't have"
 // let userIntro = 'hii this is aakrati'
-let isHappy = false
-let userIntro = 'this is' 
-  + 'username'
-  + ''
-
+let isHappy = false  
+let age = '19' 
+let a = 'me' 
+const hoursinday = 24
+let userIntro = ' this is ' 
+  + a + ' I am ' + age + ' years old '
