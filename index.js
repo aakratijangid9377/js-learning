95b8ee092be4d2f40b7a9a2f9b685da6510677c1
aakratijangid9377 <141710221+aakratijangid9377@users.noreply.github.com>
2024-09-28 18:03:08 +0000

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/ console.log(firstname)
+// console.log(firstname)
 // var firstname = 'Aakrati'
 // let lastname = 'jangid'
 // let age = 19
 // const yearofbirth = 2005
 // const userInput = prompt("enter your name")
 // console.log(userinput)
+
 const message = "hii this is aakrati , glad to see you here"
 
